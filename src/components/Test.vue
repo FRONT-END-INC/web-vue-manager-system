7ddfefd93947e52ad3369d0892fae68e06eab7f3
wangyufeng <wangyf@haierubic.com>
2017-03-08 15:22:33 +0800

    vue-cle生成的脚手架，配合semanticUI

---
+++ src/components/Test.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="semantic-component">
+    <div class="ui selection dropdown semanticDropDown">
+      <input type="hidden" name="gender" v-model="selected">
+      <i class="dropdown icon"></i>
+      <div class="default text">Gender</div>
+      <div class="menu">
+        <div class="item" :data-value="item.Value"
+             v-for="item in items"
+             @click="changeSelection(item)">
+          {{ item.Gender }}
+        </div>
+      </div>
+    </div>
+    <pre>{{ JSON.stringify(selectedItem) }}</pre>
+  </div>
+</template>
+
+<script>
+  import semantic from 'semantic'
+  import semanticUI from 'semanticUI'
+  export default {
+    data () {
+      return {
+        items : [
+          {Gender: 'Male', Value: 1},
+          {Gender: 'Female', Value: 0}
+        ],
+        selected : '',
+        selecteditem: {}
+      }
+    },
+    methods: {
+      changeSelection(item) {
+        this.selectedItem = item
+        this.selected = item.Value
+      }
+    },
+    mounted () {
+      this.selecteditem = {}
+      $(this.$el).find('.semanticDropDown').dropdown()
+    }
+  }
+</script>
+
+<style></style>
